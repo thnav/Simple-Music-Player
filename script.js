c1d5b74baeca353a23ba5ce6845ec00ec6ba1d13
thnav <tharunavanjana.dev@gmail.com>
2025-06-18 20:47:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const audio = document.getElementById("audio");
+
+function playAudio() {
+    audio.play();
+}
+
+function pauseAudio() {
+    audio.pause();
+}
+
+function stopAudio() {
+    audio.pause();
+    audio.currentTime = 0;
+}
